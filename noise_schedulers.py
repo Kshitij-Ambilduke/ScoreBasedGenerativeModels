264a2d4c5d5002dc90232010bef9ac9d5ca66134
Nemanja Vujadinovic <vujadinovic01@gmail.com>
2025-12-11 01:38:43 +0100

    updated file

--- noise_schedulers.py
+++ noise_schedulers.py
@@ -23,14 +23,14 @@ def get_sigma_scheduled(sigma_1, sigma_L, L, schedule_type="geometric"):
     return sigmas
 
 def plot_sigma_over_schedulers(sigma_1, sigma_L, L, schedule_types, use_log=False):
-    schedule_types = ['geometric', 'linear', 'quadratic', 'sqrt', 'sigmoid']
+    # schedule_types = ['geometric', 'linear', 'quadratic', 'sqrt', 'sigmoid']
     for schedule_type in schedule_types:
         sigmas = get_sigma_scheduled(sigma_1, sigma_L, L, schedule_type=schedule_type)
         sigmas = np.log(sigmas) if use_log else sigmas
-        plt.plot(torch.arange(1, L+1), sigmas, label=schedule_type)
-    plt.title("Noise Schedule Over Time for Different Schedules")
-    plt.xlabel("Level index i")
-    plt.ylabel("Noise schedules")
+        plt.plot(torch.arange(1, L+1), sigmas, label=schedule_type, marker='o')
+    plt.title("Discrete variance scheduling methods")
+    plt.xlabel("Level i")
+    plt.ylabel("Noise level σ_i" + (" (log scale)" if use_log else ""))
     plt.legend()
     if use_log:
         plt.savefig("results/sigma_log_comparison.png")
@@ -47,7 +47,9 @@ if __name__ == "__main__":
     sigma_1=1.0
     sigma_L=0.01
     L=10
-    schedule_types = ['geometric', 'linear', 'quadratic', 'sqrt', 'sigmoid']
+    # schedule_types = ['geometric', 'linear', 'quadratic', 'sqrt', 'sigmoid']
+    schedule_types = ['geometric', 'linear', 'sqrt']
+    plot_sigma_over_schedulers(sigma_1, sigma_L, L, schedule_types)
 
 
     
